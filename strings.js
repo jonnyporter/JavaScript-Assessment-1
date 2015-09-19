fe04044130454a84b967bb00b7d3b445b240ea87
Jonathan Porter <myctrring@gmail.com>
2015-09-18 21:06:38 -0600

    made an alternative solution to question 1 in strings.js using a for loop

--- strings.js
+++ strings.js
@@ -4,6 +4,19 @@ var sentence = 'When i went to the mall i bought a pair of shoes.';
 var rep = sentence.replace(/ i |i /gi, function (match) {
 	 	return match.toUpperCase(); 
 	 });
+	 
+// or
+
+var sentence = 'When i went to the mall i bought a pair of shoes.';
+
+var replace = function(){
+	var words = sentence.split(' ');
+	for(var i = 0; i < sentence.length; i++){
+		words[i] = 'I';
+	}
+	return sentence.join();
+}
+	 
 //2. The history of JavaScript is interesting but how many times does the letter a appear? Find a way to count both upper and lower case A's
 
 var jsHistory = 'JavaScript, not to be confused with Java, was created in 10 days in May 1995 by Brendan Eich, then working at Netscape and now of Mozilla. JavaScript was not always known as JavaScript: the original name was Mocha, a name chosen by Marc Andreessen, founder of Netscape. In September of 1995 the name was changed to LiveScript, then in December of the same year, upon receiving a trademark license from Sun, the name JavaScript was adopted. This was somewhat of a marketing move at the time, with Java being very popular around then.';
